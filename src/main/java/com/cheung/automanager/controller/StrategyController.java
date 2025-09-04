73a538dd4384aa2089cb1c718cb87d1b3227faae
cheung <zhangjingyu@thinkingdata.cn>
2025-09-04 14:19:44 +0800

    init

--- src/main/java/com/cheung/automanager/controller/StrategyController.java
+++ src/main/java/com/cheung/automanager/controller/StrategyController.java
@@ -23,6 +23,19 @@ public class StrategyController {
         return ResponseEntity.ok(strategyService.load());
     }
 
+        /**
+     * 执行策略接口
+     *
+     * @param sync 是否同步执行，默认为true
+     * @param dataFileName 数据文件名，默认为"XSHG_5min_600977.csv"
+     * @param startIndex 起始索引，默认为0
+     * @param lockBackNum 回溯数量，默认为200
+     * @param predLenNum 预测长度数量，默认为40
+     * @param showResult 是否渲染结果，默认为true
+     * @param printAllResult 是否打印所有结果，默认为false
+     * @return ResponseEntity<?> 策略执行结果的响应实体
+     * @throws Exception 执行过程中可能抛出的异常
+     */
     @GetMapping("/exec")
     @ApiOperation(value = "执行策略", position = 1)
     public ResponseEntity<?> exec(@RequestParam(defaultValue = "true") boolean sync,
@@ -32,6 +45,7 @@ public class StrategyController {
                                   @RequestParam(defaultValue = "40") Integer predLenNum,
                                   @RequestParam(defaultValue = "true") boolean showResult,
                                   @RequestParam(defaultValue = "false") boolean printAllResult) throws Exception {
+        // 构建策略执行参数对象
         KronosVo kronosVo = new KronosVo()
                 .setSync(sync)
                 .setDataFileName(dataFileName)
@@ -42,4 +56,5 @@ public class StrategyController {
                 .setPrintAllResult(printAllResult);
         return ResponseEntity.ok(strategyService.exec(kronosVo));
     }
+
 }
